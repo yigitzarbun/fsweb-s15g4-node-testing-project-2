d2d641f531a5608df0f54d61e74dc90f313be093
Yigit Zarbun <yzarbun@gmail.com>
2023-03-09 14:53:39 +0300

    ilk test başarılı

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,3 +1,22 @@
+const db = require("./../../data/dbConfig");
+const hobbitsModel = require("./hobbits-model");
+
 test("test environment testing olarak ayarlı", () => {
-  expect(process.env.NODE_ENV).toBe("test");
+  expect(process.env.NODE_ENV).toBe("testing");
+});
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db.seed.run();
+});
+
+describe("[1] getAll", () => {
+  test("tüm hobbitler geliyor mu", async () => {
+    const hobbits = await hobbitsModel.getAll();
+    expect(hobbits).toBeDefined();
+  });
 });
